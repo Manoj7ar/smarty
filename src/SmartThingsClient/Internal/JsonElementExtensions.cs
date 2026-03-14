eef10d2c368d09be49a3285cf9d19635fb708cda
Manoj Kumar <86283161+Manoj7ar@users.noreply.github.com>
2026-03-14 19:21:00 +0000

    feat(client): add JSON element helpers

---
+++ src/SmartThingsClient/Internal/JsonElementExtensions.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+
+namespace SmartThingsClient.Internal;
+
+internal static class JsonElementExtensions
+{
+    public static bool TryGetProperty(JsonElement element, string propertyName, out JsonElement value)
+    {
+        if (element.ValueKind == JsonValueKind.Object)
+        {
+            foreach (var property in element.EnumerateObject())
+            {
+                if (string.Equals(property.Name, propertyName, StringComparison.OrdinalIgnoreCase))
+                {
+                    value = property.Value;
+                    return true;
+                }
+            }
+        }
+
+        value = default;
+        return false;
+    }
+}
